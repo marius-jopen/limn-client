83229f83bacae3445465e5c0a4e18dd98db5fcb2
Marius Jopen <mail@mariusjopen.com>
2024-08-24 12:18:34 +0200

    added nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<nav>
+    <a href="/generate-image-1111-local">Generate Image 1111 Local</a>
+    <a href="/generate-image-1111-runpod">Generate Image 1111 RunPod</a>
+</nav>
+
+<main>
+    <slot></slot>
+</main>
