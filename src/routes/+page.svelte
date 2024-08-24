3f41cfb0a3750bd115bf3c604eb82bb309208c5a
Marius Jopen <mail@mariusjopen.com>
2024-08-24 11:03:06 +0200

    separated the image1111 local thing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,3 @@
-<script>
-    let generatedImageUrl = '';
-    let imageRequest = {
-        prompt: '',
-        steps: 20,
-        width: 1024,
-        height: 1024
-    };
-    
-    async function generateImage() {
-        const response = await fetch('http://localhost:4000/generate-image', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(imageRequest) // Send the entire object
-        });
-        
-        const data = await response.json();
-        generatedImageUrl = data.imageUrl; // Adjusted to match the response from your server
-    }
-</script>
-
 <main>
-    <h1>Stable Diffusion Image Generator</h1>
-    <input
-        type="text"
-        bind:value={imageRequest.prompt}
-        placeholder="Enter your prompt"
-    />
-    <button on:click={generateImage}>Generate</button>
-    
-    {#if generatedImageUrl}
-        <img src={generatedImageUrl} alt="Generated Image" />
-    {/if}
+    <a href="/generate-image-1111-local">Generate Image 1111 Local</a>
 </main>
---
+++ src/routes/generate-image-1111-local/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    let generatedImageUrl = '';
+    let imageRequest = {
+        prompt: '',
+        steps: 20,
+        width: 1024,
+        height: 1024
+    };
+    
+    async function generateImage() {
+        const response = await fetch('http://localhost:4000/generate-image-1111-local', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(imageRequest) // Send the entire object
+        });
+        
+        const data = await response.json();
+        generatedImageUrl = data.imageUrl; // Adjusted to match the response from your server
+    }
+</script>
+
+<main>
+    <h1>Generate Image 1111 Local</h1>
+    <input
+        type="text"
+        bind:value={imageRequest.prompt}
+        placeholder="Enter your prompt"
+    />
+    <button on:click={generateImage}>Generate</button>
+    
+    {#if generatedImageUrl}
+        <img src={generatedImageUrl} alt="Generated Image" />
+    {/if}
+</main>
