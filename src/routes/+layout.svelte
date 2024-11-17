8020f033c7f3222c192a5bf6c2c47e897a1bb233
Marius Jopen <mail@mariusjopen.com>
2024-11-17 18:53:17 +0100

    password

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,42 @@
 <script>
     import "../app.css";
     import Navigation from "$lib/layout/navigation.svelte";
+    
+    let isAuthenticated = false;
+    let password = '';
+    
+    function checkPassword() {
+        // Very simple password check - obviously not secure!
+        if (password === 'limn123') {
+            isAuthenticated = true;
+        }
+    }
 </script>
 
-<div>
-    <Navigation />
-    
-    <main class="lg:pl-72">
-        <slot></slot>
-    </main>
-</div>
+{#if isAuthenticated}
+    <div>
+        <Navigation />
+        
+        <main class="lg:pl-72">
+            <slot></slot>
+        </main>
+    </div>
+{:else}
+    <div class="h-screen flex items-center justify-center">
+        <div class="p-8 bg-white rounded shadow-md">
+            <h1 class="text-xl mb-4">Enter Password</h1>
+            <input 
+                type="password" 
+                bind:value={password}
+                class="border p-2 mb-4 w-full"
+                placeholder="Password"
+            />
+            <button 
+                on:click={checkPassword}
+                class="bg-blue-500 text-white px-4 py-2 rounded w-full"
+            >
+                Submit
+            </button>
+        </div>
+    </div>
+{/if}
