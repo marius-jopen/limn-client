a827e913295a8c4787b2e3055f136cc5aae6a8df
Marius Jopen <mail@mariusjopen.com>
2025-01-24 13:34:35 +0100

    a1111 works

--- src/routes/testing/a1111/+page.svelte
+++ src/routes/testing/a1111/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import A1111HealthCheck from '$lib/services/a1111/health-check.svelte';
+    import A1111Run from '$lib/services/a1111/run.svelte';
 </script>
 
 <A1111HealthCheck />
+<A1111Run />
