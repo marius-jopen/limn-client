45b532af3aaa87287a9c44b84881f6f4b4aa903d
Marius Jopen <mail@mariusjopen.com>
2025-03-06 10:25:12 +1300

    cleanup comfyuiflux

--- src/routes/studio/comfyui-flux/+page.svelte
+++ src/routes/studio/comfyui-flux/+page.svelte
@@ -14,46 +14,34 @@
 
 
 <div class="p-4">
-    <h1>ComfyUI Flux</h1>
+    <h1>
+        ComfyUI Flux
+    </h1>
 
     <div class="flex gap-4 flex-col md:flex-row">
         <div class="md:w-1/2">
             <ComfyuiRun 
-            service="comfyui" 
-            workflow_name="comfyui-flux"
-            ui_config={UI_CONFIG}
-            workflow={WORKFLOW}
+                service="comfyui" 
+                workflow_name="comfyui-flux"
+                ui_config={UI_CONFIG}
+                workflow={WORKFLOW}
             />
+
+            <StatusTable 
+                workflow_name="comfyui-flux"
+                label="ComfyUi Status" 
+                id="comfyui-status" 
+            />
+            <LogViewer id="log-viewer" label="Generation Logs" />
         </div>
         
         <div class="md:w-1/2">
             <PreviewImages />
+            <GalleryImages 
+                type={["uploaded", "generated"]} 
+                workflow_names={["comfyui-flux"]} 
+                defaultImagesPerRow={8}
+            />        
         </div>
     </div>
-
-    <div class="grid grid-cols-2 gap-4">
-        <div>
-            <h2>Status</h2>
-            <StatusTable 
-                service="comfyui" 
-                label="ComfyUI Flux Status" 
-                id="comfyui-flux-status" 
-            />
-        </div>
-
-        <!-- <HealthCheck service="comfyui" /> -->
-    </div>
-    
-    <div class="flex gap-4 flex-col md:flex-row">
-        <LogViewer id="log-viewer" label="Generation Logs" />
-        <JsonViewer label="Complete Response" />
-    </div>
-
-    <!-- <div class="flex gap-4 flex-col md:flex-row">
-        <LogViewer id="log-viewer" label="Generation Logs" {logs} {status} {runpodStatus} />
-        <JsonViewer label="Complete Response" data={runpodStatus} />
-    </div> -->
-    
-    <GalleryImages type="uploaded" />
-    <GalleryImages workflow_name="comfyui-flux" />
 </div>
