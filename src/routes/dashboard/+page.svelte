e8e0013df2fc293cac8efa3b3fe477a158ae9067
Marius Jopen <mail@mariusjopen.com>
2025-03-26 23:51:59 +0100

    dashbaord

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
     import UserInfoBasic from '$lib/supabase/userarea/UserinfoBasic.svelte';
 </script>
 
@@ -9,4 +9,22 @@
     </h1>
     
     <UserInfoBasic />
+</div> -->
+
+<script>
+    import DeforumRun from '$lib/runpod/api/DeforumRun.svelte';  
+    import UI_CONFIG from '$lib/workflows/deforum/DeforumLatentShiftConfig.json';
+    import WORKFLOW from '$lib/workflows/deforum/DeforumLatentShift.json';
+    import ContinuousDeforum from '$lib/supabase/studio/latent-shift/ContinuousDeforum.svelte';
+</script>
+
+<div class="pt-16">
+    <DeforumRun 
+        workflow_name="deforum-latent-shift"
+        ui_config={UI_CONFIG}
+        workflow={WORKFLOW}
+        inputLayout="controller-latent-shift"
+    />
+
+    <ContinuousDeforum workflow_names={["deforum-latent-shift"]} />
 </div>
