869147672bb45616e9798171c58b8807990f47f4
Marius Jopen <mail@mariusjopen.com>
2025-03-27 11:58:13 +0100

    forward to dashboard

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,21 @@
     import { onMount } from 'svelte';
     import { supabase } from '$lib/supabase/helper/SupabaseClient';
     import { initializeAuth } from '$lib/supabase/helper/StoreSupabase';
+    import { goto } from '$app/navigation';
 
     // Export the data prop here
     export let data;
     
-    onMount(() => {
+    const protectedFromAuthenticatedUsers = ['/', '/login', '/register'];
+
+    onMount(async () => {
         initializeAuth(supabase);
+        const { data } = await supabase.auth.getSession();
+        
+        if (data.session && protectedFromAuthenticatedUsers.includes(window.location.pathname)) {
+            console.log('Authenticated user accessing protected route, redirecting to dashboard');
+            goto('/dashboard');
+        }
     });
     
     // Now you can access data.user, data.isAdmin, and data.appSource in all pages
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
+    import { supabase } from '$lib/supabase/helper/SupabaseClient';
+
+    onMount(async () => {
+        const { data } = await supabase.auth.getSession();
+        
+        if (data.session) {
+            console.log('User is logged in, redirecting to dashboard');
+            goto('/dashboard');
+        }
+    });
+</script>
+
 <div class="min-h-screen flex items-center justify-center">
     <div class="flex flex-col items-center justify-center">
         <div class="text-4xl md:text-6xl font-bold -mt-10 text-center pb-12">
