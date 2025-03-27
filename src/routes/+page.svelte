4da0c88d9e470d1e3151832e63d6a85e8f21e38d
Marius Jopen <mail@mariusjopen.com>
2025-03-27 11:52:21 +0100

    home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,10 @@
-<h1 class="px-4 py-4 pt-12">
-    Home
-</h1>
+<div class="min-h-screen flex items-center justify-center">
+    <div class="flex flex-col items-center justify-center">
+        <div class="text-4xl md:text-6xl font-bold -mt-10 text-center pb-12">
+            🔮 
+        </div>
+        <div class="text-xl md:text-4xl font-bold -mt-10 text-center">
+            Explore the Latent Space
+        </div>
+    </div>
+</div>
